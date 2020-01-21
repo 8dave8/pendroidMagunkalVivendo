523fa6dea85b1e16bbeaa4a4b071f0daffc54bb8
8dave8 <43006387+8dave8@users.noreply.github.com>
2020-01-21 13:12:49 +0100

    update

---
+++ Stealth/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+    public class EnemyAI : MonoBehaviour {
+        public Transform[] points;
+        public int destPoint = 0;
+        void Start () {
+            GotoNextPoint();
+        }
+    void GotoNextPoint() {
+        if (points.Length == 0) return;
+        transform.Translate(points[destPoint].position*Time.deltaTime);
+    }
+    void Update () {
+        GotoNextPoint();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("wrrrrryyyyyyyyyy");
+        if(col.gameObject.tag == "Point")
+        {
+            Debug.Log("POINTT");
+            destPoint = (destPoint+1) % points.Length;
+        }
+    }
+}
